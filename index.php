ecb5dcfc9f8c6219c277b0359059249726497b2a
suaveguy <tshepo@openlearn.co.za>
2019-09-03 03:38:29 -0700

    index.php and style

---
+++ index.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Tutorial Sample Page</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<body data-spy="scroll" data-target=".navbar" data-offset="50">
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navabar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-mg-auto">
+            <li class="nav-item ">
+                <a class="nav-link" href="#home">Home<span class="sr-only">(current)</span></a>
+            </li> 
+             <li class="nav-item">
+             <a class="nav-link" href="#explore">Explore>/a> 
+                        </a>
+             </li>
+               <li class="nav-item">
+                   <a class="nav-link" href="#adventure">Adventure</a>
+               </li>
+               <li class="nav-item">
+                   <a class="nav-link" href="#blog">Blog</a>
+               </li>
+               <li class="nav-item">
+                   <a class="nav-link" href="#contact">Contact</a>
+               </li>
+        </ul>
+    </div>
+    </div>
+    </nav>
+    </header>
+    
+    <div class="jumbotron.jumbotron-fluid height100p banner" id="home">
+        <div class="container h100">
+            <div class="contentBox h100">
+                
+            </div>
+            <h1 class="display-4">Fluid jumbotron</h1>
+            <p class="lead">This is a modifiedjumbotron that occupies the entire horizontal space of its parent.</p>
+            
+        </div>
+    </div>  
+    <section class="sec1" id="explore">
+        <div class="container">
+            <div class="row">
+                <div class="offset-sm-2 col-sm-8">
+                    <div class="headerText text-center">
+                        <h2>explore the world</h2>
+                        <p>the world is at stake god is angry at his creatures </p>
+                    </div>
+                    
+                </div> 
+            </div>
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="placeBox">
+                        <div class="imgBx">
+                            <img src="images/img1." class="img-fluid">
+                            
+                        </div>
+                        <div class="content">
+                            <h3>Place Name<br><span>Country Name</span></h3>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div class="col-sm-4">
+                    <div class="placeBox">
+                        <div class="imgBx">
+                            <img src="images/img1." class="img-fluid">
+                            
+                        </div>
+                        <div class="content">
+                            <h3>Place Name<br><span>Country Name</span></h3>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div class="col-sm-4">
+                    <div class="placeBox">
+                        <div class="imgBx">
+                            <img src="images/img1." class="img-fluid">
+                            
+                        </div>
+                        <div class="content">
+                            <h3>Place Name<br><span>Country Name</span></h3>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="sec2" id="adventure">
+        <div class="container h100">
+            <div class="contentBox h100">
+                <div>
+                    <h1> adventure is everywhere</h1>
+                    <p>the ipsum isnthe web designer of all times</p>
+                    <a href="#" class="btn btnD1">Rea more..</a>
+                </div>
+            </div>
+            
+        </div>
+    </section>
+    <section class="blog" id ="blog">
+        <div class="container">
+            <div class="row">
+                <div class="offset-sm-2 col-sm-8">
+                    <div class="headerText">
+                        <h2>Our latest Post</h2>
+                        <p>the ipsum isnthe web designer of all times</p>
+                    </div> 
+                </div>    
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="blogpost">
+                        <div class="imgBx">
+                      <img  src="images/img.1jpg" class="img-fluid">      
+                        </div>
+                        <div class="content">
+                            <h1>tshepo blog</h1>
+                            <p>the ipsum isnthe web designer of all times</p>
+                        </div>
+                    </div>
+                </div>
+                     <div class="col-sm-6">
+                    <div class="blogpost">
+                        <div class="imgBx">
+                      <img  src="images/img.1jpg" class="img-fluid">      
+                        </div>
+                        <div class="content">
+                            <h1>tshepo blog</h1>
+                            <p>the ipsum isnthe web designer of all times</p>
+                        </div>
+                    </div>
+                </div>
+                     <div class="col-sm-6">
+                    <div class="blogpost">
+                        <div class="imgBx">
+                      <img  src="images/img.1jpg" class="img-fluid">      
+                        </div>
+                        <div class="content">
+                            <h1>tshepo blog</h1>
+                            <p>the ipsum isnthe web designer of all times</p>
+                            <a href="#" class="btn btnD2">Read More</a> 
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                </div>
+                     <div class="col-sm-6">
+                    <div class="blogpost">
+                        <div class="imgBx">
+                      <img  src="images/img.1jpg" class="img-fluid">      
+                        </div>
+                        <div class="content">
+                            <h1>tshepo blog</h1>
+                            <p>the ipsum isnthe web designer of all times</p>
+                            <a href="#" class="btn btnD2">Read More</a> 
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                </div>
+                     <div class="col-sm-6">
+                    <div class="blogpost">
+                        <div class="imgBx">
+                      <img  src="images/img.1jpg" class="img-fluid">      
+                        </div>
+                        <div class="content">
+                            <h1>tshepo blog</h1>
+                            <p>the ipsum isnthe web designer of all times</p>
+                            <a href="#" class="btn btnD2">Read More</a> 
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                </div>
+                     <div class="col-sm-6">
+                    <div class="blogpost">
+                        <div class="imgBx">
+                      <img  src="images/img.1jpg" class="img-fluid">      
+                        </div>
+                        <div class="content">
+                            <h1>tshepo blog</h1>
+                            <p>the ipsum isnthe web designer of all times</p>
+                            <a href="#" class="btn btnD2">Read More</a> 
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+    <section class="contact" id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="headerText text-center">
+                        <h2>contact us</h2>
+                        <p>thank for filling the form will get back to you</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row clearfix">
+                <div class="0ffset-sm-2 col-sm-8">
+                    <form>
+                      <div class="form-group">
+                          <label>name</label> 
+                          <input type="text" name="" class="" form-control="">
+                </div>
+                        <div class="form-group">
+                          <label>Phone</label> 
+                          <input type="text" name="" class="" form-control="">
+                </div>
+                        <div class="form-group">
+                          <label>Email</label> 
+                          <input type="text" name="" class="" form-control="">
+                </div>
+                        <div class="form-group">
+                          <label>Message</label> 
+                          <textarea class="form-control textarea" name=""></textarea>
+                        </div> 
+                        <div class="form-group text-center">
+                            <button class="btn btnD1">Send</button>
+                        </div>
+                    </form>
+            </div>
+        </div> 
+    </section>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <ul class="sci">
+                        <li><a href="#"><i class="fa fa-facebook"></i></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></i></a></li>
+                                        <li><a href="#"><i class="fa fa-google-plus"></i></i></a></li>
+                                                <li><a href="#"><i class="fa fa-linkedin"></i></i></a></li>
+                                                        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                    </ul> 
+                    <p class="cpryt">&copy copyright 2019 Nature | template by me tshepo</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+             
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script> 
+<script type="text/javascript">
+    $(document).scroll(function()){
+        $('.navbar').toggleClass('scrolled',$(this)).
+                scrollTop() > $('.navbar').height())
+        
+    }
+</body>
+</html>
+
+
+
